fdd1259013e50f13facffc622e397f6add86a210
marioGame <tommylo8247@gmail.com>
2025-10-20 00:34:48 -0400

    Print Portfolio and Added text file

--- main.cpp
+++ main.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
 
 using std::cin;
 using std::cout; 
 using std::endl;
+using std::ifstream;
+using std::string;
 
 int main()
 {
-    cout<<"Hello, Stock Portfolio Tracker!"<<endl;
+    ifstream stockFile("portfolio.txt");
+    if(!stockFile){
+        cout<<"Could not open portfolio.txt"<<endl;
+        return 1;
+
+    }
+
+    string stock;
+    double price, total = 0;
+
+    cout<<"Your portfolio: "<<endl;
+    while(stockFile >> stock >> price){
+        cout<<stock<<": $"<<price<<endl;
+        total += price;
+    }
+
+    cout<<"Total portfolio value: $"<<total<<endl;
 
     return 0;
 }
